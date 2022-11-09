663ed173789e4c5b1d541adde03b78e868288951
Julian Salcedo Torres <jsalcedo218@gmail.com>
2022-11-09 17:20:01 -0500

    Create expression_matters.kt

---
+++ codewars/8 kyu/expression_matters.kt
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5ae62fcf252e66d44d00008e/kotlin
+
+fun expressionsMatter(a: Int, b: Int, c: Int): Int {
+    return listOf(a * b * c, a + b + c, (a + b) * c, (a * (b + c))).max()!!
+}
+
+fun main() {
+    println(expressionsMatter(5, 3, 2))
+}
